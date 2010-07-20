b50167c13d79c77eab4fc5b3bb5721ff617318c0
liesegang <none@none>
2010-07-20 12:04:14 +0000

    Updating prereq script to work on Mac.
    --HG--
    rename : Code/Angel/linux-prereqs.sh => Code/Angel/nix-prereqs.sh
    extra : convert_revision : svn%3A18234216-3d68-4496-97ee-3aadfc93d375/trunk%40390

---
+++ Code/Angel/nix-prereqs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This script will attempt to install any necessary prerequsites on Unix
+#  systems. At the moment it only supports Fedora (13), Ubuntu (10.4) and
+#  Mac OS X (10.6). 
+# 
+# On all systems, it should be run with sudo or root privileges. 
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script requires root privileges to run."
+   exit 1
+fi
+
+if   [ -f /etc/fedora-release ] ; then
+	DIST='Fedora'
+elif [ -f /etc/lsb-release ] ; then
+	DINFO=`cat /etc/lsb-release | grep DISTRIB_ID | sed s/.*=//`
+	if [ $DINFO == 'Ubuntu' ] ; then
+		DIST='Ubuntu'
+	fi
+elif [ "`uname`" == 'Darwin' ] ; then
+  DIST='Darwin'
+fi
+
+if   [ "$DIST" == 'Fedora' ] ; then
+	yum -y install gcc-c++ make swig mesa-libGLU-devel DevIL-ILUT-devel\
+		readline-devel libXrandr-devel freetype-devel
+elif [ "$DIST" == 'Ubuntu' ] ; then
+	apt-get -y install build-essential swig libglu1-mesa-dev\
+		libreadline-dev libdevil-dev libxrandr-dev libfreetype6-dev\
+		joystick
+elif [ "$DIST" == 'Darwin' ] ; then
+	if [ "`which port`" != "" ] ; then
+		if [ "`port installed swig | grep currently`" == "" ] ; then
+			port install swig
+		fi
+	else
+		echo "MacPorts is not installed -- please download and install from <http://macports.org>."
+		exit 1
+	fi
+	if [ ! -d /System/Library/Extensions/360Controller.kext ] ; then
+		hdiutil attach ../Tools/Mac360/360ControllerInstall.0.09.dmg
+		installer -target / -pkg /Volumes/360ControllerInstall.0.09/Install360Controller.pkg
+		hdiutil detach /Volumes/360ControllerInstall.0.09
+		echo "You should restart your machine to get access to the 360 Controller."
+	fi
+else
+  echo "This is not a recognized Linux or UNIX distribution. Currently we support Fedora, Ubuntu, and OS X."
+fi
+
