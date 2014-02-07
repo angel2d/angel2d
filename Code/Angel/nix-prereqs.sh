4f04e302338b84328bb4bda5317fa73ee37bd154
Shane Liesegang <shane@techie.net>
2014-02-07 00:06:53 -0800

    Updating nix prereqs script

--- Code/Angel/nix-prereqs.sh
+++ Code/Angel/nix-prereqs.sh
@@ -24,16 +24,16 @@ elif [ "`uname`" == 'Darwin' ] ; then
 fi
 
 if   [ "$DIST" == 'Fedora' ] ; then
-	yum -y install gcc-c++ make cmake swig mesa-libGLU-devel readline-devel\
+	yum -y install gcc-c++ make cmake mesa-libGLU-devel readline-devel\
 		DevIL-ILUT-devel libXrandr-devel freetype-devel\
 		openal-soft-devel libvorbis-devel libpng-devel libXi-devel
 elif [ "$DIST" == 'Ubuntu' ] ; then
-	apt-get -y install build-essential cmake swig libglu1-mesa-dev\
+	apt-get -y install build-essential cmake libglu1-mesa-dev\
 		libreadline-dev libdevil-dev libxrandr-dev libfreetype6-dev\
 		joystick libopenal-dev libvorbis-dev libpng12-dev ncurses-dev\
-		libxi-dev
+		libxi-dev xorg-dev
 elif [ "$DIST" == 'Debian' ] ; then
-	apt-get -y install build-essential cmake swig libglu1-mesa-dev\
+	apt-get -y install build-essential cmake libglu1-mesa-dev\
 		libreadline-dev libdevil-dev libxrandr-dev libfreetype6-dev\
 		joystick libopenal-dev libvorbis-dev libpng12-dev\
 		libncurses5-dev libxi-dev xorg-dev libglu1-mesa-dev
@@ -41,27 +41,6 @@ elif [ "$DIST" == 'Darwin' ] ; then
 	if [ "`which make`" == "" ] ; then
 		echo "The Xcode command line tools are not installed."
 		echo "Use the Downloads section of the Xcode preferences to install them."
-		exit 1
-	fi
-
-	SWIG_INSTALLED=false
-	if [ "`which brew`" != "" ] ; then
-		if [ "`brew list | grep swig`" == "" ] ; then
-			su $SUDO_USER -c "brew install swig"
-		fi
-		SWIG_INSTALLED=true
-	elif [ "`which port`" != "" ] ; then
-		if [ "`port installed swig-lua | grep currently`" == "" ] ; then
-			port install swig-lua
-		fi
-		SWIG_INSTALLED=true
-	fi
-	if [ $SWIG_INSTALLED != true ] ; then
-		echo "Couldn't install swig because neither Homebrew nor MacPorts was available."
-		echo "Please either download and install from:"
-		echo "	<http://mxcl.github.com/homebrew/>"
-		echo "		or"
-		echo "	<http://macports.org>"
 		exit 1
 	fi
 
